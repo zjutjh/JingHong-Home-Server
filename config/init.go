c08b96c406f1927e0b632db1f131c873f8c35bfd
FinleyGe <m13203533462@163.com>
2022-08-23 16:23:38 +0800

    update: edit config.example.yaml

--- config/init.go
+++ config/init.go
@@ -18,26 +18,12 @@ type database struct {
 	DbName   string `mapstructure:"db_name"`
 }
 
-type email struct {
-	Sender   string `mapstructure:"sender"`
-	Receiver string `mapstructure:"receiver"`
-	Pwd      string `mapstructure:"pwd"`
-	SmtpAddr string `mapstructure:"smtp_addr"`
-	SmtpPort int    `mapstructure:"smtp_port"`
-}
-type jwt struct {
-	Issuer  string `mapstructure:"issuer"`
-	Secret  string `mapstructure:"secret"`
-	Expires int    `mapstructure:"expires"`
-}
 type ConfigData struct {
 	Server   server   `mapstructure:"server"`
 	Database database `mapstructure:"database"`
 	Secret   string   `mapstructure:"secret"`
 	Secret2  string   `mapstructure:"secret2"`
-	Email    email    `mapstructure:"email"`
 	Dev      bool     `mapstructure:"dev"`
-	Jwt      jwt      `mapstructure:"jwt"`
 }
 
 var Config ConfigData
